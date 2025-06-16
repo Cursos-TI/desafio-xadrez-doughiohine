6b644444ee409736f012a26ffbf18702cb88c0ca
Douglas Hiohine <193243396+doughiohine@users.noreply.github.com>
2025-06-16 12:21:16 -0300

    Adicionado o movimento do Cavalo ao jogo

--- DESAFIO_XADRE_NOVATO.C
+++ DESAFIO_XADRE_NOVATO.C
@@ -32,8 +32,25 @@ do {
     printf("Esquerda\n");
     contadorRainha++;
     }while (contadorRainha <= casasRainha);
+printf("\n");
 
+// Movimento do Cavalo
+printf("Movimento do Cavalo\n");
+int passosBaixo = 2; // duas casas para baixo
+int passosEsquerda = 1; // uma casa para a esquerda
 
+for(int i = 1; i <= passosBaixo; i++) {
+    printf("Baixo\n");
+    // dentro do loop for, usamos o while para simular a parte final do L
+    if (i == passosBaixo){
+        int j = 1;
+        while(j<= passosEsquerda) {
+            printf("Esquerda\n");
+            j++;
+        }
+    }
+}
     return 0;
+
 
 }
