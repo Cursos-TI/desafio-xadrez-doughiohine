349d10f61f1abb89316610d83596165bdf8f8768
Douglas Hiohine <193243396+doughiohine@users.noreply.github.com>
2025-06-16 11:53:10 -0300

    Subindo arquivos do projeto de xadrez novato

---
+++ test.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    // Movimento da Torre (linha reta para a direita)
+    printf("Movimento da Torre:\n");
+
+    int casasTorre = 5;
+    for (int i = 1; i <= casasTorre; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    // Movimento do Bispo (diagonal para cima e direita)
+    printf("Movimento do Bispo:\n");
+
+    int casasBispo = 5;
+    int contadorBispo = 1;
+    while (contadorBispo <= casasBispo) {
+        printf("Cima Direita\n");
+        contadorBispo++;
+    }
+
+    printf("\n");
+
+    // Movimento da Rainha (linha reta para a esquerda)
+    printf("Movimento da Rainha:\n");
+
+    int casasRainha = 8;
+    int contadorRainha = 1;
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= casasRainha);
+
+    return 0;
+}
